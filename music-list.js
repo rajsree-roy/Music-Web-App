566da5efd5fa7d4a495d1a11468c7d5dda9b5c09
Rajsree Roy <114103654+rajsree-roy@users.noreply.github.com>
2023-07-30 21:25:16 +0600

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,34 @@
+let allMusic = [
+
+    {
+        name: "Night Changes",
+        artist: "One Direction",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Mon Ronger Achor",
+        artist: "Tahsin Ahmed",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Titli",
+        artist: "Chinmayi Sripada-Gopi Sunder",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Ranjhaana",
+        artist: "Jaswinder Singh-Shiraz Uppal",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Katakuti Khela",
+        artist: "Shaan-Shreya Ghoshal",
+        img: "music-5",
+        src: "music-5"
+    },
+    
+    ];    
